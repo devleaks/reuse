912491b22856dd1da7aff4e5fe5c910eebadbae2
Pierre M <pierre@Mac-de-Pierre.local>
2015-10-13 16:08:13 +0200

    Clean up model

--- common/components/Donnerie.php
+++ common/components/Donnerie.php
@@ -19,23 +19,37 @@ class Donnerie extends Component
 	const DEFAULT_EXCLUSIVITY = 28;	// days
 	const DEFAULT_THEME_LOCATION = '@frontend/themes';
 
+	/** Configuration variables */
 	public $id;
 	public $donnerie;
 	public $exclusivity = self::DEFAULT_EXCLUSIVITY;
 
+
+    /**
+     * @inheritdoc
+     */
 	public function init() {
 		parent::init();
 		$this->donnerie = DonnerieModel::findOne(['key' => $_SERVER['SERVER_NAME']]);
 	}
 	
+    /**
+     * @return number Donnerie Id
+     */
 	public function getId() {
 		return $this->donnerie ? $this->donnerie->id : null;
 	}
 	
+    /**
+     * @return string Donnerie (Display) Name
+     */
 	public function getName() {
 		return $this->donnerie ? $this->donnerie->name : Yii::t('app', 'une donnerie');
 	}
 	
+    /**
+     * @return Theme|null The Donnerie's Theme, if any.
+     */
 	public function getTheme($location = self::DEFAULT_THEME_LOCATION) {
 		if($this->donnerie) {
 			if($this->donnerie->theme) {
